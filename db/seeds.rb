94db9daa4c8aab6bbe12a1e9ee1df41dd6e675f5
Alexander Popov <alex.val.popov@gmail.com>
2017-04-20 16:58:50 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+john = User.create(name: 'John', active: true)
+merry = User.create(name: 'Merry', active: true)
+deactivated = User.create(name: 'Deactivated', active: false)
+
+johns_post = john.posts.create(title: "John's post")
+merrys_post = merry.posts.create(title: "Merry's post")
+deactivated_post = deactivated.posts.create(title: "Deactivated's post")
+
+johns_post.comments.create(user: john)
+johns_post.comments.create(user: merry)
+johns_post.comments.create(user: deactivated)
+
+merrys_post.comments.create(user: john)
+merrys_post.comments.create(user: merry)
+merrys_post.comments.create(user: deactivated)
+
+deactivated_post.comments.create(user: john)
+deactivated_post.comments.create(user: merry)
+deactivated_post.comments.create(user: deactivated)
